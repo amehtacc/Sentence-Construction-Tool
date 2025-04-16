1ec252947313bda7b027f940becbc8e9c36ab240
Aryan Mehta <mehtaaryan2001@gmail.com>
2025-04-16 13:05:52 +0530

    timer function

--- src/hooks/useTimer.js
+++ src/hooks/useTimer.js
@@ -1,23 +1,27 @@
 import { useEffect, useState } from 'react'
 
-function useTimer(currentIndex, handleNext) {
+function useTimer(currentIndex, handleNext, isActive) {
     const [timer, setTimer] = useState(30)
     
     useEffect(()=> {
+        if (!isActive) return;
+
         setTimer(30);
         const interval = setInterval(() => {
             setTimer((prev) => prev - 1)
         }, 1000)
 
         return () => clearInterval(interval);
-    }, [currentIndex])
+    }, [currentIndex, isActive])
 
     useEffect(() => {
+        if (!isActive) return;
+        
         if(timer === 0) {
             handleNext()
         }
         return
-    }, [timer])
+    }, [timer, isActive])
 
   return {timer};
 }
