8d07014ea9e1993f8a51f091f800339933b58386
Aryan Mehta <mehtaaryan2001@gmail.com>
2025-04-16 13:10:36 +0530

    wrap in QuizProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
-
+import { QuizProvider } from "./contexts/QuizContext.jsx";
 
 function App() {
-  const location = useLocation()
+  const location = useLocation();
   const isQuiz = location.pathname === "/quiz";
 
   return (
     <>
-      {!isQuiz ? <Navbar /> : ''}
-      <Outlet />
+      <QuizProvider>
+        {!isQuiz ? <Navbar /> : ""}
+        <Outlet />
+      </QuizProvider>
     </>
   );
 }
